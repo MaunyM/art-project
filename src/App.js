32e466c5c401fedf6923d3277e9532120067b7dc
Matthieu <matthieu.mauny@gmail.com>
2020-03-28 16:45:45 +0100

    fix : syntax error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Amplify, { Analytics, Storage, API } from 'aws-amplify';
+import {API} from 'aws-amplify';
 
 let post = async () => {
   console.log('calling api');
@@ -26,26 +26,26 @@ let list = async () => {
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
         <p> Pick a file</p>
-        <button onClick={this.post}>POST</button>
-        <button onClick={this.get}>GET</button>
-        <button onClick={this.list}>LIST</button>
-    </div>
+        <button onClick={post}>POST</button>
+        <button onClick={get}>GET</button>
+        <button onClick={list}>LIST</button>
+      </div>
   );
 }
 
