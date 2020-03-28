49e59692fff3838173e8a3963cc7026e91abe63f
Matthieu <matthieu.mauny@gmail.com>
2020-03-28 16:44:22 +0100

    feature : init art api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Amplify, { Analytics, Storage, API } from 'aws-amplify';
+
+let post = async () => {
+  console.log('calling api');
+  const response = await API.post('api', '/art', {
+    body: {
+      id: '1',
+      name: 'hello amplify!'
+    }
+  });
+  alert(JSON.stringify(response, null, 2));
+};
+let get = async () => {
+  console.log('calling api');
+  const response = await API.get('api', '/art/object/1');
+  alert(JSON.stringify(response, null, 2));
+};
+let list = async () => {
+  console.log('calling api');
+  const response = await API.get('api', '/art/1');
+  alert(JSON.stringify(response, null, 2));
+};
 
 function App() {
   return (
@@ -19,6 +41,10 @@ function App() {
           Learn React
         </a>
       </header>
+        <p> Pick a file</p>
+        <button onClick={this.post}>POST</button>
+        <button onClick={this.get}>GET</button>
+        <button onClick={this.list}>LIST</button>
     </div>
   );
 }
